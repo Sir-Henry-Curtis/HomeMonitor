2852e3bbf9cdf2513f11041e9c5aeb32f66cba0d
Sir-Henry-Curtis <jlbrian7@gmail.com>
2013-10-22 18:48:52 -0400

    current files
    currently working through xively using a dht22 for temp and humidity
    board is for a beagle bone black, but current setup is with an arduino
    ethernet and project board.

---
+++ HomeMonitor.py
@@ -0,0 +1,92 @@
+# -*- coding: utf-8 -*-
+import xively
+import smtplib
+import datetime
+import sys
+import time
+import xml.etree.ElementTree as etree
+
+
+class Gmail():
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+        self.SMTP_SERVER = 'smtp.gmail.com'
+        self.SMTP_PORT = 587
+        self.session = None
+
+    def login(self):
+        self.session = smtplib.SMTP(self.SMTP_SERVER, self.SMTP_PORT)
+        self.session.ehlo()
+        self.session.starttls()
+        self.session.ehlo
+        login = self.session.login(self.username, self.password)
+        if login:
+            return True
+        else:
+            return False
+
+    def createHeader(self, subject, recipient):
+        header = ["from: " + self.username,
+        "subject: " + subject,
+        "to: " + recipient,
+        "mime-version: 1.0",
+        "content-type: text/html"]
+        header = "\r\n".join(header)
+        return header
+
+    def sendMail(self, subject, recipients, body):
+        login = self.login()
+        if login:
+            for recipient in recipients:
+                header = self.createHeader(subject, recipient)
+                message = header + "\r\n\r\n" + body
+                sent = self.session.sendmail(self.username, recipient, message)
+                if not sent:
+                    print "Email could not be sent to " + recipient + "!"
+            self.session.quit()
+            self.session = None
+        else:
+            print "You could not be logged in!"
+
+
+class HomeMonitor():
+    def __init__(self):
+        self.XIVELY_API_KEY = "sRnw8yFV0oIDOWYTh0OeR0dGYslIXYbKmUm3ZvAMvqaMOqcT"
+        self.XIVELY_FEED_ID = 397908926
+        self.feed = None
+        self.DEBUG = False
+
+    def getTemp(self):
+        try:
+            self.getFeed()
+            datastream = self.feed.datastreams.get("humidity1")  # 'humidity1 is correct here for now I mislabeled it on the arduino'
+            if self.DEBUG:
+                print "Found Temperature Stream."
+            return datastream
+        except:
+            if self.DEBUG:
+                print "Could not get temp datastream from Xively"
+
+    def getHumidity(self):
+        try:
+            self.getFeed()
+            datastream = self.feed.datastreams.get("temp1")  # 'temp1 is correct here for now I mislabeled it on the arduino'
+            if self.DEBUG:
+                print "Found Humidity Stream."
+            return datastream
+        except:
+            if self.DEBUG:
+                print "Could not get humidity datastream from Xively"
+
+    def getFeed(self):
+        api = xively.XivelyAPIClient(self.XIVELY_API_KEY)
+        self.feed = api.feeds.get(self.XIVELY_FEED_ID)
+
+    def currentTemp(self):
+        temp = self.getTemp()
+        return temp.current_value
+
+    def checkTemp(self, previous, current, gmail, recipients):
+        if previous != current:
+            gmail.sendMail("Change in Temperature", recipients, "There was a change in temperature. The previous temperature reading was " + str(previous) + " and the current reading is " + str(current) + ".")
